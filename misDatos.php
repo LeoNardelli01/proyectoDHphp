bbc4606182772627ba0e12044a671a248fd796ff
Leonel Nardelli <ricosur@gmail.com>
2017-10-01 00:41:01 -0300

    subiendo cambios anteriores, sino no me deja hacer pull

--- misDatos.php
+++ misDatos.php
@@ -5,6 +5,7 @@
    <head>
       <meta charset="utf-8">
       <title>Mis Datos - HS - </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="css/header.css">
       <link rel="stylesheet" href="css/footer.css">
       <link rel="stylesheet" href="css/fonts.css">
